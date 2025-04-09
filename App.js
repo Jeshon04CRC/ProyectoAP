2d631df33e5b22b5da3b21dbccb51f74a8b4cbe7
Félix Méndez <mariafelix2002@gmail.com>
2025-04-08 21:52:38 -0600

    Prueba estudantes

--- App.js
+++ App.js
@@ -1,47 +1,63 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './src/Screens/Login/login'; // Asegúrate de que la ruta sea correcta
-import RegisterScrreen from './src/Screens/Login/register'; // Asegúrate de que la ruta sea correcta
 
-//Modulo 1, conexiones 
-import HomePageEscuelaScreen  from './src/Screens/Module1/homePageEscuela';
+// Login y registro
+import LoginScreen from './src/Screens/Login/login';
+import RegisterScrreen from './src/Screens/Login/register';
+
+// Módulo 1
+import HomePageEscuelaScreen from './src/Screens/Module1/homePageEscuela';
 import publiOfertaScreen from './src/Screens/Module1/publiOferta';
 import listaOfertasScreen from './src/Screens/Module1/listaOfertas';
 import crearOfertaScreen from './src/Screens/Module1/crearOferta';
-import cursosEscuelaScreen from './src/Screens/Module1/cursosEscuela';  
 
-
-//Modulo de Profesores
-import HomePage from './src/Screens/Profesores/homePage';
+// Módulo Profesores
+import HomePageProfesores from './src/Screens/Profesores/homePage';
 import RegistroEdicion from './src/Screens/Profesores/registroEdicion';
-import GestionAsignaturas from './src/Screens/Profesores/gestionAsignaturas';
-import EvaluacionRetroalimentacion from './src/Screens/Profesores/evaluacionRetroalimentacion'; 
+
+
+
+
+
+// Módulo Estudiantes
+import HomePageEstudiantes from './src/Screens/Estudiantes/homePageEstudiantes';
+
+
+
+
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  // Return the main navigation container with stack navigator
   return (
     <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomePageEstudiantes" screenOptions={{ headerShown: false }}>
 
-        <Stack.Navigator initialRouteName="login">
-
-        {/* Login and Registration Screens */}
+        {/* Login */}
         <Stack.Screen name="login" component={LoginScreen} />
         <Stack.Screen name="registroPage" component={RegisterScrreen} />
 
-        {/* Module 1 Screens */}
+        {/* Módulo 1 */}
         <Stack.Screen name="homePageEscuela" component={HomePageEscuelaScreen} />
         <Stack.Screen name="publiOferta" component={publiOfertaScreen} />
         <Stack.Screen name="listaOfertas" component={listaOfertasScreen} />
         <Stack.Screen name="crearOferta" component={crearOfertaScreen} />
-        <Stack.Screen name="cursosEscuela" component={cursosEscuelaScreen} />
 
-        {/* Module Profesores*/}
-        <Stack.Screen name="HomePageProfesores" component={HomePage} />
+        {/* Módulo Profesores */}
+        <Stack.Screen name="HomePageProfesores" component={HomePageProfesores} />
         <Stack.Screen name="registroEdicion" component={RegistroEdicion} />
-        <Stack.Screen name="gestionAsignaturas" component={GestionAsignaturas} />
-        <Stack.Screen name="evaluacionRetroalimentacion" component={EvaluacionRetroalimentacion} />
+
+
+
+
+        {/* Módulo Estudiantes */}
+        <Stack.Screen name="HomePageEstudiantes" component={HomePageEstudiantes} />
+
+
+
+
 
 
 
---
+++ src/Style/Estudiantes/homePageEstudiantes.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  scrollContainer: {
+    padding: 20
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  subSectionTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginTop: 20,
+    marginBottom: 10
+  },
+  menuContainer: {
+    marginBottom: 20
+  },
+  menuItem: {
+    backgroundColor: '#e3e3e3',
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 5
+  },
+  menuItemText: {
+    fontSize: 16
+  }
+});
